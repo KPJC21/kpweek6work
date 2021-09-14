8892a22e5d5a18310cbcae35e02ef6a0ac884d20
KPJC21 <89233888+KPJC21@users.noreply.github.com>
2021-09-14 15:28:34 -0700

    Update [id].js

--- pages/[id].js
+++ pages/[id].js
@@ -23,17 +23,17 @@ export async function getStaticPaths() {
 export default function Entry({ itemData }) {
   return (
     <Layout>
-      <article class="card col-6">
-        <div class="card-body">
-          <h5 class="card-title">{itemData.name}</h5>
-          <h6 class="card-subtitle mb-2 text-muted">{itemData.phone}</h6>
-          <p class="card-text">{itemData.birthdate}</p>
-          <a href={'mailto:' + itemData.email} class="card-link">{itemData.email}</a>
-          <h4 class="card col-7">{itemData.person2.name}</h4>
-          <h5 class="card-title">{itemData.person2.phone}</h5>
-          <p class="card-text">{itemData.person2.birthdate}</p>
-          <a href={'mailto:' + itemData.person2.email} class="card-link">{itemData.person2.email}</a>
-          <p class="card-text">A friend</p>
+      <article className="card col-6">
+        <div className="card-body">
+          <h5 className="card-title">{itemData.name}</h5>
+          <h6 className="card-subtitle mb-2 text-muted">{itemData.phone}</h6>
+          <p className="card-text">{itemData.birthdate}</p>
+          <a href={'mailto:' + itemData.email} className="card-link">{itemData.email}</a>
+          <h4 className="card col-7">{itemData.person2.name}</h4>
+          <h5 className="card-title">{itemData.person2.phone}</h5>
+          <p className="card-text">{itemData.person2.birthdate}</p>
+          <a href={'mailto:' + itemData.person2.email} className="card-link">{itemData.person2.email}</a>
+          <p className="card-text">A friend</p>
         </div>
       </article>
     </Layout>
