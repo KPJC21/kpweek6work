70ad0733ad5f6cf335a56345c219a8fb51cfc6fc
KPJC21 <89233888+KPJC21@users.noreply.github.com>
2021-09-15 11:50:44 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,17 +13,18 @@ export async function getStaticProps() {
   }
 }
 export default function Home({ allData }) {
-  return (
-      <Layout home>
-        <h1>List of Names</h1>
-        <div className="list-group">
-          {allData.map(({ id, name }) => (
-            <Link href={`/${id}`}>
-              <a className="list-group-item list-group-item-action">{name}</a>
-            </Link>
-          ))}
-        </div>
-      </Layout>
-  );
-}
+return (
+<Layout home>
+<h1>List of Names</h1>
+<div className="list-group">
+{allData.map((mydata) => (
 
+<Link href={`/${mydata.id}`} key={mydata.id} >
+<a className="list-group-item list-group-item-action">{mydata.name}</a>
+</Link>
+
+))}
+</div>
+</Layout>
+);
+}
